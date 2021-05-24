0844231e77b5b17d84dba3d4b2a68015136cc34b
Alexander Krumeich <alexander@krumeich.de>
2021-05-24 17:50:22 +0200

    updated n-doc version number
    Added translation and noindentafter packages

--- runmake.sh
+++ runmake.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-docker run --rm  --volume $(pwd):/data ndesign/n-doc:${engine:-4.1.0} make -j4 delivery
+docker run --rm  --volume $(pwd):/data ndesign/n-doc:${engine:-4.2.0} make -j4 delivery
 
