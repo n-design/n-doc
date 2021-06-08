454882d54b80288478e4f50f58ff9529f01f6c8c
Thomas Herlinghaus <thomas.herlinghaus@ingenico.com>
2021-06-08 14:44:11 +0200

    run docker container with current user id's

--- runmake.sh
+++ runmake.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 theversion=$(grep 'container: ndesign/n-doc:' .github/workflows/build_n-doc_template.yml | cut -d ':' -f3)
+user_id=$(id -u)
+group_id=$(id -g)
 
 if [[ "$OSTYPE" == "msys" ]]; then
 	local_dir=$(pwd | sed -e 's!/!!' | sed -e 's!/!:/!')
 else
 	local_dir=$(pwd)		
 fi
-
-docker run --rm  --volume "${local_dir}:/data" ndesign/n-doc:${engine:-$theversion} make -j${cores:-4} ${@:-delivery}
+echo  
+docker run --rm  --volume "${local_dir}:/data" --user $user_id:$group_id ndesign/n-doc:${engine:-$theversion} make -j${cores:-4} ${@:-delivery}
 
 
 
