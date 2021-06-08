164fcbcb754d489fa3ce347ea0dd87882618a3e4
Thomas Herlinghaus <thomas.herlinghaus@ingenico.com>
2021-06-08 11:10:37 +0200

    fix: build local path name for msys-env in start docker container script

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-#/bin/bash
+#!/bin/bash
 
 theversion=$(grep 'container: ndesign/n-doc:' .github/workflows/build_n-doc_template.yml | cut -d ':' -f3)
 
-docker run -d -i --name ndoc -v ${PWD}:/data ndesign/n-doc:${engine:-$theversion}
+if [[ "$OSTYPE" == "msys" ]]; then
+	local_dir=$(pwd | sed -e 's!/!!' | sed -e 's!/!:/!')
+else
+	local_dir=$(pwd)		
+fi
+
+docker run -d -i --name ndoc -v "${local_dir}:/data" ndesign/n-doc:${engine:-$theversion}
 
