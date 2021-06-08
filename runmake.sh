a507f0e7d6481d744c7ca7a60cd98ccf8746f4c6
Thomas Herlinghaus <thomas.herlinghaus@ingenico.com>
2021-06-08 11:12:26 +0200

    fix:  consider spaces in local path name

--- runmake.sh
+++ runmake.sh
@@ -3,13 +3,12 @@
 theversion=$(grep 'container: ndesign/n-doc:' .github/workflows/build_n-doc_template.yml | cut -d ':' -f3)
 
 if [[ "$OSTYPE" == "msys" ]]; then
-	local_dir=$( pwd | sed -e 's!/!!' | sed -e 's!/!:/!')
-	#theversion=latest
+	local_dir=$(pwd | sed -e 's!/!!' | sed -e 's!/!:/!')
 else
 	local_dir=$(pwd)		
 fi
 
-docker run --rm  --volume ${local_dir}:/data ndesign/n-doc:${engine:-$theversion} make -j${cores:-4} ${@:-delivery}
+docker run --rm  --volume "${local_dir}:/data" ndesign/n-doc:${engine:-$theversion} make -j${cores:-4} ${@:-delivery}
 
 
 
